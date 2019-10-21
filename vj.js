c72eefadce49949517663e9bf2a280bcbb5027c1
qrkz <56621986+qrkz@users.noreply.github.com>
2019-10-21 06:37:34 +0530

    Add files via upload

--- vj.js
+++ vj.js
@@ -1,3 +1,47 @@
+document.addEventListener(
+  "DOMContentLoaded", () => {
+      new Mmenu( "#my-menu", {
+          "extensions": [
+            "pagedim-black",
+            "theme-dark"
+          ],
+          "counters": true,
+          "navbars": [
+            {
+                "position": "top",
+                "content": [
+                  "searchfield"
+                ]
+            },
+            {
+                "position": "top",
+                "content": [
+                  "prev",
+                  "title"
+                ]
+            },
+            {
+                "position": "bottom",
+                "content": [
+                  "<a class='fas fa-envelope' href='#/'></a>",
+                  "<a class='fas fa-twitter' href='#/'></a>",
+                  "<a class='fas fa-facebook' href='#/'></a>"
+                ]
+            }
+          ]
+      }, {
+        // configuration
+        classNames: {
+            fixedElements: {
+                fixed: "fix",
+                sticky: "stick"
+            }
+        }
+    });
+  }
+);
+
+
 var galleryThumbs = new Swiper('.gallery-thumbs', {
   spaceBetween: 0,
   slidesPerView: 5,
@@ -13,3 +57,10 @@ var galleryTop = new Swiper('.gallery-top', {
     swiper: galleryThumbs
   }
 });
+
+var element = document.getElementById('back-link');
+element.setAttribute('href', document.referrer);
+element.onclick = function() {
+  history.back();
+  return false;
+}
